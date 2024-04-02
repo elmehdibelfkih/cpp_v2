3ad89bc9bc80344e5c5ec4440fe0d82ae8f6b6dc
El Mehdi Belfkih <ebelfkih@e3r5p4.1337.ma>
2024-04-02 01:27:10 +0000

    cpp08 : ex02 : main target finish

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 07:03:06 by ebelfkih          #+#    #+#             */
-/*   Updated: 2024/03/31 21:42:18 by ebelfkih         ###   ########.fr       */
+/*   Updated: 2024/04/02 01:26:48 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,22 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
+    MutantStack(){}
+    MutantStack& operator=(const MutantStack& obj)
+    {
+        if (this != &obj)
+            this->c = obj.c;
+        return *this;
+    }
+    MutantStack(const MutantStack& obj)
+    {
+        *this = obj;
+    }
+    ~MutantStack()
+    {
+        this->c.clear();
+    }
+
     typedef typename std::deque<T>::iterator iterator;
     iterator begin()
     {
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 07:03:15 by ebelfkih          #+#    #+#             */
-/*   Updated: 2024/03/31 21:26:48 by ebelfkih         ###   ########.fr       */
+/*   Updated: 2024/04/02 01:22:42 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int main()
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
+    // std::cout << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << mstack.size() << std::endl;
+    // std::cout << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
@@ -31,9 +31,20 @@ int main()
     --it;
     while (it != ite)
     {
-    std::cout << *it << std::endl;
-    ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
-    std::stack<int> s(mstack);
+    MutantStack<int> s(mstack);
+    MutantStack<int>::iterator it1 = s.begin();
+    MutantStack<int>::iterator ite1 = s.end();
+    std::cout << "----------------------------" << std::endl;
+    mstack.pop();
+    while (it1 != ite1)
+    {
+        std::cout << *it1 << std::endl;
+        ++it1;
+    }
+    
+    // std::stack<int> s(mstack);
     return 0;
 }
